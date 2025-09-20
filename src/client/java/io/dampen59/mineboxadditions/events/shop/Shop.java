a39c426452005c7be10e3b574dcd52debaaa0e73
Kevin Bartier (Dampen59) <k.kedu596@gmail.com>
2025-09-20 03:21:07 +0200

    fix(shops): mouse closes at 5AM (23000 ticks)

--- src/client/java/io/dampen59/mineboxadditions/events/shop/Shop.java
+++ src/client/java/io/dampen59/mineboxadditions/events/shop/Shop.java
@@ -4,7 +4,7 @@ public enum Shop {
     BAKERY(6000, 12000, "mineboxadditions.strings.toasts.shop.bakery.open.title", "mineboxadditions.strings.toasts.shop.bakery.open.content"),
     BUCKSTAR(10, 6000, "mineboxadditions.strings.toasts.shop.buckstar.open.title", "mineboxadditions.strings.toasts.shop.buckstar.open.content"),
     COCKTAIL(12000, 13500, "mineboxadditions.strings.toasts.shop.cocktail.open.title", "mineboxadditions.strings.toasts.shop.cocktail.open.content"),
-    MOUSE(13500, 20000, "mineboxadditions.strings.toasts.shop.mouse.open.title", "mineboxadditions.strings.toasts.shop.mouse.open.content");
+    MOUSE(13500, 23000, "mineboxadditions.strings.toasts.shop.mouse.open.title", "mineboxadditions.strings.toasts.shop.mouse.open.content");
 
     private final int startTime;
     private final int stopTime;
